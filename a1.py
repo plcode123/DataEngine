843c002b0810a61aa0ab6edf1c60dcdb537953bc
plcode123 <66989148+plcode123@users.noreply.github.com>
2020-06-16 15:07:27 +0800

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('jfl')
+
+
+# In[24]:
+
+
+sum = 0
+number = 2
+while number < 101:
+      sum = sum + number
+      number = number + 2      
+print(sum)
+
+
+# In[19]:
+
+
+sum = 0
+for number in range(2,101,2):
+    sum = sum + number
+print(sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[18]:
+
+
+sum = 0
+for number in range(2,101,2):
+    sum += number
+print(sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
